8d76a4002e0470652366731f3de793560dbdca97
Daniel Nottingham <54644738+danielnottingham@users.noreply.github.com>
2024-01-29 17:33:36 -0300

    fix: fix event_procedure update operation (#57)

--- spec/operations/event_procedures/update_spec.rb
+++ spec/operations/event_procedures/update_spec.rb
@@ -5,9 +5,8 @@ require "rails_helper"
 RSpec.describe EventProcedures::Update, type: :operation do
   describe ".result" do
     context "with valid attributes" do
-      let!(:event_procedure) { create(:event_procedure) }
-
       it "updates event_procedure" do
+        event_procedure = create(:event_procedure)
         attributes = { date: Time.zone.yesterday, payd_at: Time.zone.tomorrow }
         described_class.result(id: event_procedure.id.to_s, attributes: attributes)
 
@@ -18,13 +17,32 @@ RSpec.describe EventProcedures::Update, type: :operation do
       end
 
       it "update event_procedure association 'patient'" do
+        patient = create(:patient, name: "Old Patient")
+        event_procedure = create(:event_procedure, patient: patient)
         attributes = { patient_id: create(:patient, name: "New Patient").id }
         result = described_class.result(id: event_procedure.id.to_s, attributes: attributes)
 
         expect(result.event_procedure.reload.patient.name).to eq("New Patient")
       end
 
+      it "update event_procedure total_amount_cents" do
+        procedure = create(:procedure, amount_cents: 1000)
+        event_procedure = create(
+          :event_procedure,
+          procedure: procedure,
+          urgency: false,
+          room_type: EventProcedures::RoomTypes::WARD
+        )
+        attributes = { urgency: true, room_type: EventProcedures::RoomTypes::APARTMENT }
+
+        result = described_class.result(id: event_procedure.id.to_s, attributes: attributes)
+
+        expect(result.event_procedure.total_amount_cents).to eq(2300)
+        expect(result.event_procedure.total_amount.format).to eq("R$23.00")
+      end
+
       it "returns success" do
+        event_procedure = create(:event_procedure)
         attributes = { date: Time.zone.yesterday, payd_at: Time.zone.tomorrow }
         result = described_class.result(id: event_procedure.id.to_s, attributes: attributes)
 
