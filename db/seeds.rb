53cbd7eea944df9d596f408caee64ebdd2cc6b19
Daniel Nottingham <54644738+danielnottingham@users.noreply.github.com>
2024-03-03 15:10:10 -0300

    add admin user (#98)

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 
 User.create(email: "admin@email.com", password: "password", admin: true)
 User.create(email: "edimossilva@gmail.com", password: "edimossilva@gmail.com", admin: true)
+User.create(email: "daniel@gmail.com", password: "daniel@gmail.com", admin: true)
 
 # Rails.logger.debug "Seeding default data..."
 # Dir[Rails.root.join("db/seeds/*.rb")].each do |seed|
