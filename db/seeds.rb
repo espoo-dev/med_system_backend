910e1c96a9e4c3779ed17e9c7f7de0e102e790b2
Daniel Nottingham <54644738+danielnottingham@users.noreply.github.com>
2024-03-03 15:53:20 -0300

    fix seeds and users dashboard (#100)

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,6 @@ User.create(email: "admin@email.com", password: "password", admin: true)
 User.create(email: "edimossilva@gmail.com", password: "edimossilva@gmail.com", admin: true)
 User.create(email: "daniel@gmail.com", password: "daniel@gmail.com", admin: true)
 
-procedure = Procedure.all
-procedure.select { _1.id > 25 }.each(&:destroy)
-
-health_insurance = HealthInsurance.all
-health_insurance.select { _1.id > 9 }.each(&:destroy)
-
 # Rails.logger.debug "Seeding default data..."
 # Dir[Rails.root.join("db/seeds/*.rb")].each do |seed|
 #   load seed
