92ea581c2cd69c5c25b1d4485ee253d57e13af00
Daniel Nottingham <54644738+danielnottingham@users.noreply.github.com>
2024-03-05 06:27:47 -0300

    fix: uniqueness validation of name and address for Hospital (#106)
    * fix: uniqueness validation of name and address for Hospital
    
    * fix: test description

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -9,11 +9,18 @@ RSpec.describe Hospital do
   end
 
   describe "validations" do
-    subject { build(:hospital) }
-
-    it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
-    it { is_expected.to validate_uniqueness_of(:address).case_insensitive }
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:address) }
+
+    context "when validating name and address uniqueness" do
+      it "does not allow duplicate names and addresses" do
+        create(:hospital, name: "nome hospital", address: "Barbalha- CE")
+        new_hospital = build(:hospital, name: "nomE hospitAl", address: "Barbalha- Ce")
+
+        expect(new_hospital).not_to be_valid
+        expect(new_hospital.errors[:name]).to include("has already been taken")
+        expect(new_hospital.errors[:address]).to include("has already been taken")
+      end
+    end
   end
 end
