5c8a6c569e802af6b090bdb45236f137b4f7ece5
Edimo Silva <edimossilva@gmail.com>
2024-03-08 12:19:01 -0300

    120 - add actor to handle data formatting

---
+++ app/operations/event_procedures/dashboard_by_date_range.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module EventProcedures
+  class DashboardByDateRange < Actor
+    input :start_date_time, type: DateTime
+    input :end_date_time, type: DateTime
+
+    output :dashboard_data, type: Hash
+
+    def call
+      event_procedures = EventProcedure.date_between(start_date_time, end_date_time)
+
+      self.dashboard_data = {
+        start_date: format_date(start_date_time),
+        end_date: format_date(end_date_time),
+        days: event_procedures_by_date(event_procedures),
+        events_amount: event_procedures.count
+      }
+    end
+
+    private
+
+    def event_procedures_by_date(event_procedures)
+      result = {}
+      date_range = start_date_time..end_date_time
+
+      date_range.each do |date_time|
+        formatted_date_time = format_date(date_time)
+        result[formatted_date_time] = 0
+      end
+
+      event_procedures.each do |event_procedure|
+        formatted_date_time = format_date(event_procedure.date)
+        result[formatted_date_time] += 1
+      end
+
+      result
+    end
+
+    def format_date(date)
+      date.strftime("%d/%m/%Y")
+    end
+  end
+end
---
+++ spec/operations/event_procedures/dashboard_by_date_range_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe EventProcedures::DashboardByDateRange, type: :operation do
+  describe ".result" do
+    let(:result) { described_class.result(start_date_time:, end_date_time:) }
+
+    let(:year) { 2000 }
+    let(:month) { 6 }
+    let(:start_date_time) { DateTime.new(year, month, 1) }
+    let(:end_date_time) { DateTime.new(year, month, 5) }
+
+    before do
+      # out of range
+      create_list(:event_procedure, 1, date: start_date_time - 1.day)
+      # included on range
+      create_list(:event_procedure, 2, date: start_date_time)
+      create_list(:event_procedure, 3, date: start_date_time + 1.day)
+      create_list(:event_procedure, 4, date: end_date_time)
+      # out of range
+      create_list(:event_procedure, 5, date: end_date_time + 1.day)
+    end
+
+    it { expect(result).to be_success }
+
+    it "returns days" do
+      expect(result.dashboard_data[:days]).to eq(
+        { "01/06/2000" => 2,
+          "02/06/2000" => 3,
+          "03/06/2000" => 0,
+          "04/06/2000" => 0,
+          "05/06/2000" => 4 }
+      )
+    end
+
+    it "returns start_data" do
+      expect(result.dashboard_data[:start_date]).to eq("01/06/2000")
+    end
+
+    it "returns end_data" do
+      expect(result.dashboard_data[:end_date]).to eq(end_date_time.strftime("05/06/2000"))
+    end
+
+    it "returns events_amount" do
+      expect(result.dashboard_data[:events_amount]).to eq(9)
+    end
+  end
+end
