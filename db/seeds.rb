05c0d2eeb77597acdca8e13da768c826971f681a
Edimo Silva <edimossilva@gmail.com>
2024-03-13 10:50:47 -0300

    update event_procedures payd_at

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,6 @@
 # Not proud of, but we are using this as a place to run scripts,
 # since we can't run rake tasks or access console on render free version
 
-# set the field payd to event_procedures
-event_procedures = EventProcedure.where.not(payd_at: nil)
-event_procedures.each { _1.update(payd: true) }
-
 # Rails.logger.debug "Seeding default data..."
 # Dir[Rails.root.join("db/seeds/*.rb")].each do |seed|
 #   load seed
