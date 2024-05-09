8beff3fe945e8f11352dd4c73effe14efa7a2f36
Philipe Leandro <philipe-leandro@hotmail.com>
2024-05-09 20:47:18 -0300

    Adjust some suggestions

--- app/bin/scripts/read_procedures_pdf.rb
+++ app/bin/scripts/read_procedures_pdf.rb
@@ -19,28 +19,30 @@ module Scripts
 
     def csv_data_to_json
       file.each_slice(batch_size).with_index do |batch, batch_index|
-        procedures = { batch: { rows: [] } }
-
         batch.each_with_index do |row, index|
-          from_row_to_json(procedures, row, index, batch_index)
+          from_row_to_json(row, index, batch_index)
         end
 
-        batches_procedures_parsed(batch_index, procedures.to_json)
+        batches_procedures_parsed(batch_index, procedures_hash.to_json)
       end
     end
 
-    def from_row_to_json(json, row, row_index, batch_index)
+    def from_row_to_json(row, row_index, batch_index)
       return if row.first.blank?
 
-      json.dig(:batch, :rows) << { code: row[0], name: row[1]&.gsub("'",""), port: row[4] }
-      puts "Concluído - Batch: #{ batch_index + 1 } - linha: #{ row_index + 1 }"
+      procedures_hash.dig(:batch, :procedures) << { code: row[0], name: row[1], port: row[4] }
+      puts "DONE - Batch: #{batch_index + 1} - row: #{row_index + 1}"
     rescue StandardError => e
-      logger.warn("ERRO: #{e.message} - Batch: #{batch_index}/linha: #{row_index + 1}|Backtrace: #{e.backtrace}")
-      puts "ERRO - Confira no arquivo de log"
+      logger.warn("ERROR: #{e.message} - Batch: #{batch_index}/:row #{row_index + 1}|Backtrace: #{e.backtrace}")
+      puts "ERROR - Check log file"
     end
 
     def batches_procedures_parsed(index, data)
       File.write("batch_#{index + 1}.json", data)
+    end
+
+    def procedures_hash
+      @procedures_hash ||= { batch: { procedures: [] } }
     end
 
     def file
@@ -51,4 +53,3 @@ module Scripts
       @logger ||= Logger.new(Rails.root.join("log/populate_procedures_from_csv.log"))
     end
   end
-end
