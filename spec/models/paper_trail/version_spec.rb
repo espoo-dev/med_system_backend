f38fc8b3b3c26b23c87bbae61cb3449711901c98
Philipe Leandro <philipe-leandro@hotmail.com>
2024-05-12 13:52:19 -0300

    Split test in paper trail request spec

--- spec/models/paper_trail/version_spec.rb
+++ spec/models/paper_trail/version_spec.rb
@@ -21,40 +21,6 @@ RSpec.describe PaperTrail::Version, versioning: true do
 
     it { expect(hospital_version.count).to eq(1) }
     it { expect(last_hospital_version.object["name"]).to eq("nome hospital") }
-  end
-
-  context "when api request", type: :request do
-    let(:hospital_creation) { Hospital.all }
-    let(:hospital_created) { hospital_creation.last }
-
-    describe "POST /api/v1/hospitals" do
-      before do
-        post "/api/v1/hospitals", params: { name: "Hospital", address: "Address" },
-          headers: auth_token_for(create(:user))
-      end
-
-      it "creates hospital and not version" do
-        expect(response).to have_http_status(:created)
-        expect(hospital_creation.count).to eq(1)
-        expect(hospital_created.versions.count).to eq(0)
-      end
-    end
-
-    describe "PUT /api/v1/hospitals/:id" do
-      let(:hospital) { create(:hospital, name: "Hospital first") }
-      let(:hospital_version) { hospital.versions }
-      let(:last_hospital_version) { hospital_version.last }
-
-      before do
-        put "/api/v1/hospitals/#{hospital.id}", params: { name: "Hospital", address: "Address" },
-          headers: auth_token_for(create(:user))
-      end
-
-      it "creates hospital and version" do
-        expect(response).to have_http_status(:ok)
-        expect(hospital_version.count).to eq(1)
-        expect(last_hospital_version.object["name"]).to eq("Hospital first")
-      end
-    end
+    it { expect(last_hospital_version.object["whodunnit"]).to be_nil }
   end
 end
