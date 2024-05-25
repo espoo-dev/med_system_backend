a54be6e96790d9f14279e218880ccb61f668e8de
Philipe Leandro <philipe-leandro@hotmail.com>
2024-05-25 11:51:52 -0300

    Add script to persist procedures

--- spec/tasks/create_json_file_spec.rb
+++ spec/tasks/create_json_file_spec.rb
@@ -9,6 +9,9 @@ RSpec.describe "create_json_file" do
   let(:file_path) { "spec/fixtures/procedures_test.csv" }
   let(:files_dir) { Dir.glob("lib/data/procedures/*") }
   let(:files_selected) { files_dir.select { |file| File.file?(file) } }
+  let(:dir_path) { "lib/data/procedures" }
+  let(:run_task) { Rake::Task[task_name].invoke(file_path, 1) }
+  let(:delete_dir) { FileUtils.remove_dir(dir_path,true) }
 
   before do
     Rake.application.rake_require("tasks/create_json_file")
@@ -17,6 +20,8 @@ RSpec.describe "create_json_file" do
 
   context "when successful" do
     it {
+      delete_dir
+
       expect do
         Rake::Task[task_name].invoke(file_path, 1)
       end.to output("Procedures exported to json files\n").to_stdout
@@ -32,7 +37,7 @@ RSpec.describe "create_json_file" do
         expect(file["batch"].keys).to include("procedures")
 
         file.dig("batch", "procedures").each do |procedure|
-          expect(procedure.keys).to include("code", "port", "name")
+          expect(procedure.keys).to include("code", "port", "name", "anesthetic_port")
         end
       end
 
@@ -41,12 +46,12 @@ RSpec.describe "create_json_file" do
   end
 
   context "when the rake task is a failure" do
-    before(:each) { Rake::Task[task_name].reenable }
+    before { Rake::Task[task_name].reenable }
 
     context "when code is not valid" do
       let(:file_path_code_error) { "spec/fixtures/procedures_code_error_test.csv" }
 
-      it 'returns error' do
+      it "returns error" do
         expect do
           Rake::Task[task_name].invoke(file_path_code_error, 1)
         end.to raise_error(StandardError, "Code is not valid!")
@@ -58,7 +63,7 @@ RSpec.describe "create_json_file" do
     context "when port is not valid" do
       let(:file_path_port_error) { "spec/fixtures/procedures_port_error_test.csv" }
 
-      it 'returns error' do
+      it "returns error" do
         expect do
           Rake::Task[task_name].invoke(file_path_port_error, 1)
         end.to raise_error(StandardError, "Port is not valid!")
