2e150d7a6ab467cc67684df9b097385c154c7260
Daniel Nottingham <danielnottingham.ti@gmail.com>
2024-05-30 18:56:17 -0300

    fix: return for anesthetic port zero

--- app/operations/event_procedures/build_total_amount_cents.rb
+++ app/operations/event_procedures/build_total_amount_cents.rb
@@ -8,6 +8,7 @@ module EventProcedures
 
     APARTMENT_PERCENTAGE = 1
     URGENCY_PERCENTAGE = 0.3
+    ANESTHETIC_PORT_ZERO_AMOUNT = "3"
 
     def call
       self.total_amount_cents = base_amount_cents + apartment_amount + urgency_amount.to_i
@@ -19,12 +20,17 @@ module EventProcedures
       return event_procedure.procedure.amount_cents.to_i if event_procedure.procedure.custom?
 
       cbhpm_procedure = find_cbhpm_procedure
-      return false unless cbhpm_procedure
+      return 0 unless cbhpm_procedure
 
       port_value = find_port_value(cbhpm_procedure)
-      return false unless port_value
+      return value_for_anesthetic_port_zero(event_procedure) unless port_value
 
       port_value.amount_cents.to_i
+    end
+
+    def value_for_anesthetic_port_zero(event_procedure)
+      port = PortValue.find_by(cbhpm_id: event_procedure.cbhpm_id, anesthetic_port: ANESTHETIC_PORT_ZERO_AMOUNT)
+      port&.amount_cents.to_i 
     end
 
     def find_cbhpm_procedure
