18e2894cef8151e6b019fef0132eb854826adae9
Philipe Leandro <philipe-leandro@hotmail.com>
2024-07-13 12:47:11 -0300

    Add callback to clean patient name before save

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -6,7 +6,15 @@ class Patient < ApplicationRecord
 
   validates :name, presence: true
 
+  before_save :remove_name_whitespace
+
   def deletable?
     event_procedures.empty?
+  end
+
+  private
+
+  def remove_name_whitespace
+    self.name = name.strip
   end
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -29,4 +29,20 @@ RSpec.describe Patient do
       it { is_expected.to be_falsy }
     end
   end
+
+  describe "callbacks" do
+    context "when before_save" do
+      let(:patient) { create(:patient, name: "  Patient Name Test  ") }
+
+      context "when patient is created" do
+        it { expect(patient.name).to eq("Patient Name Test") }
+      end
+
+      context "when patient is updated" do
+        before { patient.update(name: "  New Patient Name  ") }
+
+        it { expect(patient.name).to eq("New Patient Name") }
+      end
+    end
+  end
 end
