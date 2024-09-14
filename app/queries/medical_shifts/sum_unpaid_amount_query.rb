8c653431330f38de3faf0bb9877ca5913e502d3b
Edimo Silva <edimossilva@gmail.com>
2024-09-14 08:17:59 -0300

    217: add filter by month to MedicalShifts::SumUnpaidAmountQuery

--- app/queries/medical_shifts/sum_unpaid_amount_query.rb
+++ app/queries/medical_shifts/sum_unpaid_amount_query.rb
@@ -2,15 +2,18 @@
 
 module MedicalShifts
   class SumUnpaidAmountQuery < ApplicationQuery
-    attr_reader :user_id, :relation
+    attr_reader :user_id, :month, :relation
 
-    def initialize(user_id:, relation: MedicalShift)
+    def initialize(user_id:, month: nil, relation: MedicalShift)
       @user_id = user_id
+      @month = month
       @relation = relation
     end
 
     def call
-      relation.where(user_id: user_id).where(payd: false).sum(:amount_cents)
+      query = relation.where(user_id:).where(payd: false)
+      query = query.where("EXTRACT(MONTH FROM start_date) = ?", month) if month.present?
+      query.sum(:amount_cents)
     end
   end
 end
