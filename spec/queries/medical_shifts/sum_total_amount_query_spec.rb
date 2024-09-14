afba083ba31af02af8870ac80020eb3425a551fb
Edimo Silva <edimossilva@gmail.com>
2024-09-14 08:24:58 -0300

    217: add filter by month to MedicalShifts::SumTotalAmountQuery

--- spec/queries/medical_shifts/sum_total_amount_query_spec.rb
+++ spec/queries/medical_shifts/sum_total_amount_query_spec.rb
@@ -5,12 +5,14 @@ require "rails_helper"
 RSpec.describe MedicalShifts::SumTotalAmountQuery do
   it "returns the sum of amount_cents" do
     user = create(:user)
-    _medical_shift = create(:medical_shift, amount_cents: 1000, payd: true, user: user)
-    _payd_medical_shift = create(:medical_shift, amount_cents: 2000, payd: true, user: user)
-    _unpaid_medical_shift = create(:medical_shift, amount_cents: 1500, payd: false, user: user)
+    _medical_shift = create(:medical_shift, amount_cents: 1000, payd: true, user:, start_date: "25/02/2023")
+    _payd_medical_shift = create(:medical_shift, amount_cents: 2000, payd: true, user:, start_date: "25/02/2023")
+    _unpaid_medical_shift = create(:medical_shift, amount_cents: 1500, payd: false, user:, start_date: "25/03/2023")
 
-    sum_total_amount_query = described_class.call(user_id: user.id)
+    amount_by_month = described_class.call(user_id: user.id, month: 2)
+    amount_without_month = described_class.call(user_id: user.id,)
 
-    expect(sum_total_amount_query).to eq(4500)
+    expect(amount_without_month).to eq(4500)
+    expect(amount_by_month).to eq(3000)
   end
 end
