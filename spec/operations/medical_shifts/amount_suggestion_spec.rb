d871b5f0f484667a5dab15fe862cb3932adf5b93
Edimo Silva <edimossilva@gmail.com>
2024-09-14 09:02:45 -0300

    219: create operation MedicalShifts::AmountSuggestion

---
+++ spec/operations/medical_shifts/amount_suggestion_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe MedicalShifts::AmountSuggestion, type: :operation do
+  describe '#call' do
+    context 'when there are no amounts stored' do
+      let(:user) { create(:user) }
+
+      it 'returns an empty list' do
+        result = described_class.result(scope: MedicalShift.all)
+
+        expect(result.amounts).to be_empty
+      end
+    end
+
+    context 'when there are amounts stored' do
+      let(:user) { create(:user) }
+      let(:another_user) { create(:user) }
+
+      before do
+        FactoryBot.create(:medical_shift, user:, amount_cents: 1000)
+        FactoryBot.create(:medical_shift, user:, amount_cents: 1000) # Duplicate amount
+        FactoryBot.create(:medical_shift, user:, amount_cents: 2000)
+        FactoryBot.create(:medical_shift, user:, amount_cents: 3000)
+      end
+
+      it 'returns unique amounts' do
+        result = described_class.result(scope: MedicalShift.all)
+
+        expected_amounts = [1000, 2000, 3000]
+        expect(result.amounts).to match_array(expected_amounts)
+      end
+    end
+  end
+end
