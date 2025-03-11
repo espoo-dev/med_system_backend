8babc260b5b54ee6bf68bf4e06e9248c53d2051e
Matheus Ferreira <matheusmlfg@gmail.com>
2025-03-11 10:32:24 -0300

    Refacts EventProcedures TotalAmountCents total/payd/unpaid calculations to remove code repetition

--- app/operations/event_procedures/total_amount_cents.rb
+++ app/operations/event_procedures/total_amount_cents.rb
@@ -9,54 +9,22 @@ module EventProcedures
     output :unpaid, type: String
 
     def call
-      procedures = Procedure.where(id: event_procedures.pluck(:procedure_id))
+      procedures_by_id = Procedure.where(id: event_procedures.pluck(:procedure_id)).index_by(&:id)
 
-      self.total = calculate_total(procedures)
-      self.payd = calculate_payd(procedures)
-      self.unpaid = calculate_unpayd(procedures)
+      self.total = calculate_amount(event_procedures, procedures_by_id)
+      self.payd = calculate_amount(event_procedures.select(&:payd), procedures_by_id)
+      self.unpaid = calculate_amount(event_procedures.reject(&:payd), procedures_by_id)
     end
 
     def convert_money(amount_cents)
       Money.new(amount_cents, "BRL").format
     end
 
-    def calculate_total(user_procedures)
-      procedures_by_id = {}
-      user_procedures.each do |procedure|
-        procedures_by_id[procedure.id] = procedure
+    def calculate_amount(filtered_event_procedures, procedures_by_id)
+      total_cents = filtered_event_procedures.sum do |event_procedure|
+        procedures_by_id[event_procedure.procedure_id]&.amount_cents.to_i
       end
-      total = 0
-      event_procedures.each do |event_procedure|
-        procedure = procedures_by_id[event_procedure.procedure_id]
-        total += procedure.amount_cents if procedure
-      end
-      convert_money(total)
-    end
-
-    def calculate_payd(user_procedures)
-      procedures_by_id = {}
-      user_procedures.each do |procedure|
-        procedures_by_id[procedure.id] = procedure
-      end
-      total = 0
-      event_procedures.select(&:payd).each do |event_procedure|
-        procedure = procedures_by_id[event_procedure.procedure_id]
-        total += procedure.amount_cents if procedure
-      end
-      convert_money(total)
-    end
-
-    def calculate_unpayd(user_procedures)
-      procedures_by_id = {}
-      user_procedures.each do |procedure|
-        procedures_by_id[procedure.id] = procedure
-      end
-      total = 0
-      event_procedures.reject(&:payd).each do |event_procedure|
-        procedure = procedures_by_id[event_procedure.procedure_id]
-        total += procedure.amount_cents if procedure
-      end
-      convert_money(total)
+      convert_money(total_cents)
     end
   end
 end
