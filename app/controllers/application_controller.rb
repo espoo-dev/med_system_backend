35fedf7941f75753b405be51f88dfea7f8179b02
Lucas Leandro <lukkassoliveira2215@gmail.com>
2025-04-14 15:44:09 -0300

    fixes user login redirection for admins and no admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@
 
 class ApplicationController < ActionController::Base
   before_action :set_paper_trail_whodunnit
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) ||
+      if resource.admin?
+        admin_health_insurances_path
+      else
+        api_v1_event_procedures_path
+      end
+  end
+
+  def after_sign_up_path_for(resource)
+    after_sign_in_path_for(resource)
+  end
 end
