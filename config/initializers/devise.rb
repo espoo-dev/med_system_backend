9852c8195d83c557715dc24ae84556b4400a68b6
Lucas Leandro <lukkassoliveira2215@gmail.com>
2025-05-06 14:12:48 -0300

    resolved offence rubocop

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -265,10 +265,8 @@ Devise.setup do |config|
     scope: "user,public_repo"
 
   config.omniauth :strava, ENV.fetch("STRAVA_CLIENT_ID", nil).to_i, ENV.fetch("STRAVA_CLIENT_SECRET", nil),
-    scope: "profile:read_all", token_params: {
-      client_id: ENV.fetch("STRAVA_CLIENT_ID", nil),
-      client_secret: ENV.fetch("STRAVA_CLIENT_SECRET", nil)
-    }
+    scope: "profile:read_all", token_params: { client_id: ENV.fetch("STRAVA_CLIENT_ID", nil),
+                                               client_secret: ENV.fetch("STRAVA_CLIENT_SECRET", nil) }
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
