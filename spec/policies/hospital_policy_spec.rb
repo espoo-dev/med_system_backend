9af8f79fa2e39016d1a04d4ddae9d9103387fe60
Lucas Leandro <lukkassoliveira2215@gmail.com>
2025-05-06 12:39:11 -0300

    rubocop offences resolved

--- spec/policies/hospital_policy_spec.rb
+++ spec/policies/hospital_policy_spec.rb
@@ -5,16 +5,16 @@ require "rails_helper"
 RSpec.describe HospitalPolicy do
   let(:user) { create(:user) }
   let(:hospital) { create(:hospital) }
-  
+
   describe "permissions" do
     context "when has user" do
-      subject { described_class.new(user, hospital)}
+      subject { described_class.new(user, hospital) }
 
       it { is_expected.to permit_actions(%i[index create update destroy]) }
     end
 
     context "when has no user" do
-      subject { described_class.new(nil, hospital)}
+      subject { described_class.new(nil, hospital) }
 
       it { is_expected.to forbid_actions(%i[index create update destroy]) }
     end
