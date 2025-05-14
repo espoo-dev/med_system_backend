8b4d20235027d4af7758f5221733591f14019cbf
Matheus Ferreira <matheusmlfg@gmail.com>
2025-05-14 10:46:01 -0300

    Removes unecessary destroy calls on destroy_self

--- app/operations/users/destroy_self.rb
+++ app/operations/users/destroy_self.rb
@@ -14,15 +14,7 @@ module Users
     def call
       return Result.new(false, "Wrong password") unless valid_password?
 
-      ActiveRecord::Base.transaction do
-        destroy_event_procedures!
-        destroy_medical_shifts!
-        destroy_patients!
-        destroy_procedures!
-        destroy_health_insurances!
-
-        user.destroy
-      end
+      ActiveRecord::Base.transaction { user.destroy }
 
       Result.new(true, nil)
     rescue StandardError => e
@@ -33,32 +25,6 @@ module Users
 
     def valid_password?
       user.valid_password?(@password)
-    end
-
-    def destroy_event_procedures!
-      user.event_procedures.destroy_all
-    end
-
-    def destroy_medical_shifts!
-      user.medical_shifts.destroy_all
-    end
-
-    def destroy_health_insurances!
-      user.health_insurances.destroy_all
-    end
-
-    def destroy_procedures!
-      user.procedures.destroy_all
-    end
-
-    def destroy_patients!
-      user.patients.each do |patient|
-        if patient.event_procedures.exists?(deleted_at: nil)
-          raise ActiveRecord::InvalidForeignKey, "Patient ##{patient.id} has associated procedures"
-        end
-
-        patient.destroy
-      end
     end
   end
 end
