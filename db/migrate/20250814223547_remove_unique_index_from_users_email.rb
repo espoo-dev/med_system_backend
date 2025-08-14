b548a99af8824beb5946ad75c6e21f3e8a1b4dfa
Lucas Leandro <lukkassoliveira2215@gmail.com>
2025-08-14 19:53:54 -0300

    removed validation unique email to schema

---
+++ db/migrate/20250814223547_remove_unique_index_from_users_email.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class RemoveUniqueIndexFromUsersEmail < ActiveRecord::Migration[7.1]
+  def up
+    remove_index :users, name: "index_users_on_email"
+    add_index :users, :email, name: "index_users_on_email"
+  end
+
+  def down
+    remove_index :users, name: "index_users_on_email"
+    add_index :users, :email, name: "index_users_on_email", unique: true
+  end
+end
