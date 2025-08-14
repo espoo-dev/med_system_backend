e5cec7fa4953ecb7640dcfafa2add5b7d31bf2ba
Lucas Leandro <lukkassoliveira2215@gmail.com>
2025-08-14 20:02:06 -0300

    added validatable

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :api,
+    :recoverable, :rememberable, :validatable, :api,
     :omniauthable, :confirmable, :lockable,
     omniauth_providers: %i[github strava google_oauth2]
 
@@ -16,14 +16,5 @@ class User < ApplicationRecord
   has_many :procedures, dependent: :destroy
   has_many :health_insurances, dependent: :destroy
 
-  validates :email, presence: true, format: { with: Devise.email_regexp },
-    uniqueness: { case_sensitive: false, conditions: -> { where(deleted_at: nil) } }
-
-  validates :password, presence: true, length: { minimum: 6 }, confirmation: true, if: :password_required?
-
-  private
-
-  def password_required?
-    !persisted? || !password.nil? || !password_confirmation.nil?
-  end
+  validates :email, uniqueness: { case_sensitive: false }
 end
